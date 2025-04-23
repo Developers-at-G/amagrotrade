2847584a697edf849ab0ae0a5d76a52b65a4f317
abdallah96 <gueye.amadou1996@gmail.com>
2025-04-23 22:01:40 +0200

    update contact

--- am-agro-trade/src/app/ContactForm/ContactForm.tsx
+++ am-agro-trade/src/app/ContactForm/ContactForm.tsx
@@ -106,7 +106,7 @@ const ContactForm = () => {
                   icon: (
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   ),
-                  href: 'mailto:info@amagrotrade.com',
+                  href: 'mailto:info@am-agrotradeservices.de',
                   delay: 0.5
                 }
               ].map((item, index) => (
