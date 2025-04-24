fcd92bddfdfd4885bb37d68f545c8e472c4df950
abdallah96 <gueye.amadou1996@gmail.com>
2025-04-24 22:07:36 +0200

    update contact

--- am-agro-trade/src/app/ContactForm/ContactForm.tsx
+++ am-agro-trade/src/app/ContactForm/ContactForm.tsx
@@ -93,12 +93,30 @@ const ContactForm = () => {
                 },
                 {
                   title: t('contact.info.phone'),
-                  content: '+49 (1630) 432533',
+                  content: '+49 6131 88888 02',
                   icon: (
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   ),
-                  href: 'tel:+491630432533',
+                  href: 'tel:+4961318888802',
                   delay: 0.4
+                },
+                {
+                  title: t('contact.info.phone'),
+                  content: '+49 6131 88888 03',
+                  icon: (
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                  ),
+                  href: 'tel:+4961318888803',
+                  delay: 0.5
+                },
+                {
+                  title: t('contact.info.fax'),
+                  content: '+49 6131 88888 04',
+                  icon: (
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                  ),
+                  href: 'tel:+4961318888804',
+                  delay: 0.6
                 },
                 {
                   title: t('contact.info.email'),
@@ -107,7 +125,7 @@ const ContactForm = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   ),
                   href: 'mailto:info@am-agrotradeservices.de',
-                  delay: 0.5
+                  delay: 0.7
                 }
               ].map((item, index) => (
                 <motion.div 
